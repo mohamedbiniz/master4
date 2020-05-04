b09b7d3b328cba212ec461a0858f8c43c6ffcfa7
mohamedbiniz <mohamedbiniz@gmail.com>
2020-05-04 11:25:50 +0000

    version 3 add filter

--- src/main/java/com/master4/aop/Test.java
+++ src/main/java/com/master4/aop/Test.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class Test {
     @Autowired
     private ArticleController articleController;
