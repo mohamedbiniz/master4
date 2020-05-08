11926fc952f94e999e78c0a86415bbfd2a5e28a1
mohamedbiniz <mohamedbiniz@gmail.com>
2020-05-08 16:16:44 +0000

    version 3 add filter + interceptor correct

--- src/main/java/com/master4/interceptor/LoginInterceptor.java
+++ src/main/java/com/master4/interceptor/LoginInterceptor.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// spring context
 public class LoginInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle (HttpServletRequest request,
@@ -15,14 +14,15 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
                               Object handler) throws Exception {
         RequestMapping rm = ((HandlerMethod) handler).getMethodAnnotation(
                 RequestMapping.class);
-
         boolean alreadyLoggedIn = request.getSession()
                 .getAttribute("user") != null;
+        boolean loginPageRequested = rm != null && rm.value().length > 0
+                && "login".equals(rm.value()[0]);
 
-        if (alreadyLoggedIn ) {
-            response.sendRedirect(request.getContextPath() +rm.value()[0]);
+        if (alreadyLoggedIn && loginPageRequested) {
+            response.sendRedirect(request.getRequestURI());
             return false;
-        } else if (!alreadyLoggedIn) {
+        } else if (!alreadyLoggedIn && !loginPageRequested) {
 
             response.sendRedirect(request.getContextPath() + "/login");
             return false;
