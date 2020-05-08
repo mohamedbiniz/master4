109c04b34e15d42b596e900d7b89d881a50bc5e4
mohamedbiniz <mohamedbiniz@gmail.com>
2020-05-08 15:53:27 +0000

    version 3 add filter + interceptor correct

--- src/main/java/com/master4/filter/LogFilter.java
+++ src/main/java/com/master4/filter/LogFilter.java
@@ -1,26 +1,6 @@
 package com.master4.filter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.method.HandlerMethod;
-
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.http.HttpFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import java.io.IOException;
-import java.lang.invoke.MethodHandles;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-// web container
-// request , response
 
 public class LogFilter implements Filter {
 
